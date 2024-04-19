e3553507c60c5daaa21d6c001d0fb23a5e82a1c3
webthethird <ratzbodell@gmail.com>
2024-04-19 13:23:14 -0400

    Fix typo

--- test/helpers/Utils.sol
+++ test/helpers/Utils.sol
@@ -1,14 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.16;
 
-;
-
-;
-
-;
-
-;
-
 // interfaces
 import "../../contracts/interfaces/IVault.sol";
 import "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
