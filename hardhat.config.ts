4595ba66f33538d2e6a8496b13aec6cae739763a
webthethird <ratzbodell@gmail.com>
2024-07-08 13:47:33 -0400

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,76 +10,78 @@ import "@matterlabs/hardhat-zksync-verify";
 const config: HardhatUserConfig = {
   defaultNetwork: "zkSyncSepoliaTestnet",
   paths: {
-    sources: "helpers"
+    sources: "contracts",
   },
   networks: {
     zkSyncSepoliaTestnet: {
       url: "https://sepolia.era.zksync.dev",
       ethNetwork: "https://rpc.sepolia.org",
       zksync: true,
-      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification"
+      verifyURL:
+        "https://explorer.sepolia.era.zksync.dev/contract_verification",
     },
     zkSyncMainnet: {
       url: "https://mainnet.era.zksync.io",
-      ethNetwork: "mainnet",
+      ethNetwork:
+        "https://eth-mainnet.g.alchemy.com/v2/2jMImehwbevJjXQcalI6cLgpOmu2UogF",
       zksync: true,
       verifyURL:
-        "https://zksync2-mainnet-explorer.zksync.io/contract_verification"
+        "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
     },
     dockerizedNode: {
       url: "http://localhost:3050",
       ethNetwork: "http://localhost:8545",
-      zksync: true
+      zksync: true,
     },
     inMemoryNode: {
       url: "http://127.0.0.1:8011",
       ethNetwork: "", // in-memory node doesn't support eth node; removing this line will cause an error
-      zksync: true
+      zksync: true,
     },
     hardhat: {
-      zksync: true
-    }
+      zksync: true,
+    },
   },
   zksolc: {
     version: "latest",
     settings: {
       optimizer: {
         enabled: true,
-        mode: "z"
+        mode: "z",
       },
       // find all available options in the official documentation
       // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
-      isSystem: true
-    }
+      isSystem: true,
+    },
   },
   zkvyper: {
     version: "1.3.17",
     settings: {
       // find all available options in the official documentation
       // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-vyper.html#configuration
-    }
+    },
   },
   solidity: {
     compilers: [
       {
-        version: "0.8.16"
+        version: "0.8.16",
       },
       {
-        version: "0.8.20"
-      }
-    ]
+        version: "0.8.20",
+      },
+    ],
   },
   // Currently, only Vyper 0.3.3 or 0.3.9 are supported.
   vyper: {
     compilers: [
       {
-        version: "0.3.3"
+        version: "0.3.3",
       },
       {
-        version: "0.3.10"
-      }
-    ]
-  }
+        version: "0.3.10",
+      },
+    ],
+  },
 };
 
 export default config;
